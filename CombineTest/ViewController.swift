289930a792a400aaf3e04f79d1a13d46d9757b05
= <gualtiero.frigerio@gmail.com>
2019-06-26 14:35:19 +0200

    first example

--- CombineTest/ViewController.swift
+++ CombineTest/ViewController.swift
@@ -9,31 +9,65 @@
 import UIKit
 import Combine
 
+struct ViewModel {
+    
+    @Published var username:String = ""
+    @Published var password:String = ""
+    @Published var passwordRepeat:String = ""
+    
+    var validCredential:AnyPublisher<Bool, Never> {
+        return validUsername.combineLatest(validPassword) { (validUsername, validPassword) in
+            return validUsername && validPassword
+            }.eraseToAnyPublisher()
+    }
+    
+    var validPassword:AnyPublisher<Bool, Never> {
+        return $password.combineLatest($passwordRepeat) { (password, passwordRepeat)  in
+            var isValid = false
+            isValid = (password == passwordRepeat) && password.count > 3
+            return isValid
+            }.eraseToAnyPublisher()
+    }
+    
+    var validUsername:AnyPublisher<Bool, Never> {
+        return $username
+            .debounce(for: 0.2, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map{$0.count > 3 ? true : false}
+            .eraseToAnyPublisher()
+    }
+}
 
 class ViewController: UIViewController {
+    
+    private var registerButtonSubscriber:AnyCancellable?
+    private var viewModel = ViewModel()
     
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var passwordRepeatText: UITextField!
     @IBOutlet weak var registerButton: UIButton!
     
-    var username:String = ""
-    var password:String = ""
-    var passwordRepeat:String = ""
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerButtonSubscriber = viewModel.validCredential
+            .receive(on: RunLoop.main)
+            .assign(to: \.isEnabled, on: registerButton)
         
     }
 
     @IBAction func usernameDidChange(_ sender: UITextField) {
-        
+        viewModel.username = sender.text ?? ""
     }
     
     @IBAction func passwordDidChange(_ sender: UITextField) {
+        viewModel.password = sender.text ?? ""
     }
     
     @IBAction func passwordRepeatDidChange(_ sender: UITextField) {
+        viewModel.passwordRepeat = sender.text ?? ""
     }
 }
+
+
 
