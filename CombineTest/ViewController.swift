454bd87683200a761f68c01236d03f4b7b2999ce
= <gualtiero.frigerio@gmail.com>
2019-06-26 14:36:38 +0200

    first example

--- CombineTest/ViewController.swift
+++ CombineTest/ViewController.swift
@@ -9,35 +9,6 @@
 import UIKit
 import Combine
 
-struct ViewModel {
-    
-    @Published var username:String = ""
-    @Published var password:String = ""
-    @Published var passwordRepeat:String = ""
-    
-    var validCredential:AnyPublisher<Bool, Never> {
-        return validUsername.combineLatest(validPassword) { (validUsername, validPassword) in
-            return validUsername && validPassword
-            }.eraseToAnyPublisher()
-    }
-    
-    var validPassword:AnyPublisher<Bool, Never> {
-        return $password.combineLatest($passwordRepeat) { (password, passwordRepeat)  in
-            var isValid = false
-            isValid = (password == passwordRepeat) && password.count > 3
-            return isValid
-            }.eraseToAnyPublisher()
-    }
-    
-    var validUsername:AnyPublisher<Bool, Never> {
-        return $username
-            .debounce(for: 0.2, scheduler: RunLoop.main)
-            .removeDuplicates()
-            .map{$0.count > 3 ? true : false}
-            .eraseToAnyPublisher()
-    }
-}
-
 class ViewController: UIViewController {
     
     private var registerButtonSubscriber:AnyCancellable?
