c6fafc3c254a5647bb0119b592e7777d69c88a0c
= <gualtiero.frigerio@gmail.com>
2019-06-27 16:45:39 +0200

    added rest client

---
+++ CombineTest/RESTClient.swift
@@ -0,0 +1,38 @@
+//
+//  RESTClient.swift
+//  CombineTest
+//
+//  Created by Gualtiero Frigerio on 27/06/2019.
+//  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+enum RESTClientError : Error {
+    case error(error:String)
+}
+
+class RESTClient {
+    class func getData(atURL url:URL) -> AnyPublisher<Data, RESTClientError> {
+        let session = URLSession.shared
+        return AnyPublisher { subscriber in
+            let task = session.dataTask(with: url) { data, response, error in
+                if let err = error {
+                    subscriber.receive(completion: .failure(.error(error: err.localizedDescription)))
+                }
+                else {
+                    if let data = data {
+                        _ = subscriber.receive(data)
+                        subscriber.receive(completion: .finished)
+                    }
+                    else {
+                        let unknownError = RESTClientError.error(error: "Unknown error")
+                        subscriber.receive(completion: .failure(unknownError))
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
