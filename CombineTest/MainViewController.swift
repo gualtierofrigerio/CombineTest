3cca8ae421e3ee29485113a6be9a145a00f21b8f
= <gualtiero.frigerio@gmail.com>
2019-06-27 16:47:36 +0200

    added users button action

--- CombineTest/MainViewController.swift
+++ CombineTest/MainViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    let dataSource = DataSource(baseURL:"https://jsonplaceholder.typicode.com")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showUsersTap(_ sender: Any) {
+        _ = dataSource.getUsersWithMergedData().sink { users in
+            DispatchQueue.main.async {
+                let usersVC = UsersTableViewController()
+                usersVC.setUsers(users)
+                self.navigationController?.pushViewController(usersVC, animated: true)
+            }
+        }
+        // get users and show UsersTableViewController
     }
-    */
-
 }
