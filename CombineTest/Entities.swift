84bd1be629d9f4bd14822419278d23c8ba58218b
= <gualtiero.frigerio@gmail.com>
2019-06-27 12:14:16 +0200

    added entities structs

---
+++ CombineTest/Entities.swift
@@ -0,0 +1,48 @@
+//
+//  Entities.swift
+//  CombineTest
+//
+//  Created by Gualtiero Frigerio on 27/06/2019.
+//  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
+//
+
+import Foundation
+
+struct Album:Codable {
+    var id:Int
+    var title:String
+    var userId:Int
+    
+    var pictures:[Picture]?
+    
+    mutating func addPicture(_ picture:Picture) {
+        if pictures == nil {
+            pictures = [Picture]()
+        }
+        pictures?.append(picture)
+    }
+}
+
+struct Picture:Codable {
+    var id:Int
+    var albumId:Int
+    var title:String
+    var url:String
+    var thumbnailUrl:String
+}
+
+struct User:Codable {
+    var id:Int
+    var email:String
+    var name:String
+    var username:String
+    
+    var albums:[Album]?
+    
+    mutating func addAlbum(_ album:Album) {
+        if albums == nil {
+            albums = [Album]()
+        }
+        albums?.append(album)
+    }
+}
