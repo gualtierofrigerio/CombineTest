abfec8693a945727771bc178ce57ffa3dd09e936
= <gualtiero.frigerio@gmail.com>
2019-06-27 12:14:41 +0200

    added users table

---
+++ CombineTest/UsersTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  UsersTableViewController.swift
+//  CombineTest
+//
+//  Created by Gualtiero Frigerio on 27/06/2019.
+//  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
