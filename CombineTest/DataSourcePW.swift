6d542d5aca92dcc6c900ce3621453a0149dfdd54
= <gualtiero.frigerio@gmail.com>
2019-07-02 10:06:24 +0200

    added property wrapper

---
+++ CombineTest/DataSourcePW.swift
@@ -0,0 +1,60 @@
+//
+//  DataSourcePW.swift
+//  CombineTest
+//
+//  Created by Gualtiero Frigerio on 01/07/2019.
+//  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
+//
+
+import Combine
+
+// Alternative implementation of the DataSource class using property wrappers
+// Some of the logic is moved to RemoteEntity so this class only has to
+// combine the 3 publishers and return the merged users
+
+class DataSourcePW {
+    
+    @RemoteEntity(entity:.Album) var albums:[Album]
+    @RemoteEntity(entity:.Picture) var pictures:[Picture]
+    @RemoteEntity(entity:.User) var users:[User]
+    
+    func getUsersWithMergedData() -> AnyPublisher<[User], Never> {
+        return Publishers.Zip3($pictures.publisher, $albums.publisher, $users.publisher)
+            .map {
+                let mergedAlbums = DataSourcePW.mergeAlbums($1, withPictures: $0)
+                return DataSourcePW.mergeUsers($2, withAlbums: mergedAlbums)
+            }
+            .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Class functions
+
+extension DataSourcePW {
+    
+    class func mergeAlbums(_ albums:[Album], withPictures pictures:[Picture]) -> [Album] {
+        var albumsWithPictures = [Album]()
+        for var album in albums {
+            for picture in pictures {
+                if picture.albumId == album.id {
+                    album.addPicture(picture)
+                }
+            }
+            albumsWithPictures.append(album)
+        }
+        return albumsWithPictures
+    }
+    
+    class func mergeUsers(_ users:[User], withAlbums albums:[Album]) -> [User] {
+        var usersWithAlbums = [User]()
+        for var user in users {
+            for album in albums {
+                if album.userId == user.id {
+                    user.addAlbum(album)
+                }
+            }
+            usersWithAlbums.append(user)
+        }
+        return usersWithAlbums
+    }
+}
---
+++ CombineTest/EntitiesPropertyWrapper.swift
@@ -0,0 +1,37 @@
+//
+//  EntitiesPropertyWrapper.swift
+//  CombineTest
+//
+//  Created by Gualtiero Frigerio on 01/07/2019.
+//  Copyright © 2019 Gualtiero Frigerio. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+@propertyWrapper
+struct RemoteEntity<T> where T:Decodable {
+    let url:URL?
+    let baseURLString = "https://jsonplaceholder.typicode.com"
+    var defaultValue:T
+    
+    var publisher:AnyPublisher<T, Never> {
+        guard let url = url else {
+            return Publishers.Just(self.defaultValue).eraseToAnyPublisher()
+        }
+        return RESTClient.getData(atURL: url)
+            .decode(type: T.self, decoder: JSONDecoder())
+            .catch { error in
+                Publishers.Just(self.defaultValue).eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    init(entity:Entity) {
+        self.defaultValue = [] as! T
+        self.url = URL(string:baseURLString + entity.endPoint)
+        self.value = defaultValue
+    }
+    
+    var value:T
+}
