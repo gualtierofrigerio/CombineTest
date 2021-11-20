13dd5ec1c2784188eddc977bbd359257eb95ce38
Gualtiero <gualtiero.frigerio@gmail.com>
2021-11-20 09:52:23 +0100

    first commit

---
+++ CombineTest/PicturesCollectionVM.swift
@@ -0,0 +1,38 @@
+//
+//  PicturesCollectionVM.swift
+//  CombineTest
+//
+//  Created by Gualtiero Frigerio on 20/11/21.
+//  Copyright © 2021 Gualtiero Frigerio. All rights reserved.
+//
+
+import Foundation
+
+class PicturesCollectionVM {
+    weak var viewController: PicturesCollectionVC?
+    
+    var pictures: [PictureWithImage] {
+        get {
+            loadedPictures
+        }
+    }
+    
+    @available(iOS 15.0.0, *)
+    func loadPictures(_ pictures: [Picture]) {
+        let picturesWithImages: [PictureWithImage]  = pictures.map { PictureWithImage(title: $0.title, imageUrl: $0.url) }
+        let pictureLoader = PicturesLoader(withPictures: picturesWithImages)
+        Task {
+            do {
+                for try await picture in pictureLoader {
+                    loadedPictures.append(picture)
+                    await viewController?.collectionView.reloadData()
+                }
+            }
+            catch {
+                print("error while iterating on images")
+            }
+        }
+    }
+    
+    private var loadedPictures: [PictureWithImage] = []
+}
---
+++ CombineTest/PicturesLoader.swift
@@ -0,0 +1,41 @@
+//
+//  PicturesLoader.swift
+//  CombineTest
+//
+//  Created by Gualtiero Frigerio on 20/11/21.
+//  Copyright © 2021 Gualtiero Frigerio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@available(iOS 15.0.0, *)
+class PicturesLoader: AsyncSequence, AsyncIteratorProtocol {
+    typealias Element = PictureWithImage
+    
+    init(withPictures pictures: [PictureWithImage]) {
+        self.pictures = pictures
+    }
+    
+    func next() async throws -> Element? {
+        await getNextPicture()
+    }
+    
+    func makeAsyncIterator() -> PicturesLoader {
+        self
+    }
+    
+    private var pictures: [PictureWithImage] = []
+    
+    private func getNextPicture() async -> PictureWithImage? {
+        guard let nextPicture = pictures.popLast(),
+              let url = URL(string: nextPicture.imageUrl) else { return nil }
+        if let data = await RESTClient.getData(atURL: url) {
+            let image = UIImage(data: data)
+            var picture = nextPicture
+            picture.image = image
+            return picture
+        }
+        return nil
+    }
+}
